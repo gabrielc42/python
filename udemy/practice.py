349cd26f4db3b1d2123be1d4031f72e9ce040ded
gabrielc42 <clark.gabe97@gmail.com>
2022-12-06 10:57:35 -0600

    strings exercises

---
+++ udemy/practice.py
@@ -0,0 +1,19 @@
+# pigeon = 'pigeon'
+# carrot = 'carrot'
+# cobalt = 'cobalt'
+
+# print("Here is an animal, a vegetable, and a mineral. \n{} \n{} \n{}".format(pigeon, carrot, cobalt))
+
+# input = input("Please type something and press enter: ")
+# print("You entered: \n", input)
+
+text = input('What did the cat say?')
+text_length = len(text)
+
+print('           {}'.format('_' * text_length))
+print('         < {} >'.format(text))
+print('           {}'.format('-' * text_length))
+print('          /')
+print('  /\_/\  /')
+print(' ( o.o )')
+print('  > ^ <')
